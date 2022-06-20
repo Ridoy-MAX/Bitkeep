c54addc75353cf1a1f7f440483f90354628e1457
Ridoy-MAX <pou4w98y@gmail.com>
2022-06-20 14:58:22 +0600

    see page

--- js/custom.js
+++ js/custom.js
@@ -95,42 +95,42 @@ $('.slider-one').slick({
 
 
 
-$('.banner-slider1').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  dots:false,
-  arrows:false,
-  autoplaySpeed: 2000,
-  nextArrow:'<i class="fas fa-chevron-left  next-arrow"></i>',
-  prevArrow:'<i class="fas fa-chevron-right  previous-arrow"></i>',
+// $('.banner-slider1').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: false,
+//   dots:false,
+//   arrows:false,
+//   autoplaySpeed: 2000,
+//   nextArrow:'<i class="fas fa-chevron-left  next-arrow"></i>',
+//   prevArrow:'<i class="fas fa-chevron-right  previous-arrow"></i>',
  
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
+//   responsive: [
+//     {
+//       breakpoint: 992,
+//       settings: {
     
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
+//       }
+//     },
+//     {
+//       breakpoint: 768,
+//       settings: {
    
-      }
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
+//       }
+//     },
+//     {
+//       breakpoint: 576,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         dots: false
+//       }
+//     }
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ]
+// });
 
 
 
